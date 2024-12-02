702aee173022f24fe0a0b79e4187eefb198453d4
Nihal Dhakal <nihaldhakal41@gmail.com>
2024-12-02 17:32:47 +0545

    Added a dashboard and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get "dashboards/index"
+  get "/homes", to: "homes#index"
   devise_for :users, skip: [:sessions]
   as :user do
     get 'signin', to: 'devise/sessions#new', as: :new_user_session
     post 'signin', to: 'devise/sessions#create', as: :user_session
-    match 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
+    delete 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
-  root "homes#index"
-  get "/homes", to: "homes#index"
+  root "dashboards#index"
+  get "/dashboards", to: "dashboards#index"
 end

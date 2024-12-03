d5be83dd94b36151d8dfff4b0b8e44f0c2403b42
Nihal Dhakal <nihaldhakal41@gmail.com>
2024-12-03 17:54:52 +0545

    Controller logic for income and expense)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
+  # Dashboard Routes
   get "dashboards/index"
+  get "/dashboards", to: "dashboards#index"
+
+  # Money Tracker Routes
+  resources :money_trackers
   get "/homes", to: "homes#index"
+
+  # Devise Routes
   devise_for :users, skip: [:sessions]
   as :user do
     get 'signin', to: 'devise/sessions#new', as: :new_user_session
     post 'signin', to: 'devise/sessions#create', as: :user_session
     delete 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
+
+  # Root Path
   root "dashboards#index"
-  get "/dashboards", to: "dashboards#index"
 end
